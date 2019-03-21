1eb3b507499633c6b232d7128adbac834a5114bd
David Rodríguez <deivid.rodriguez@riseup.net>
2019-03-21 15:12:55 +0100

    Fix the cops

--- lib/bundler/version.rb
+++ lib/bundler/version.rb
@@ -1,10 +1,7 @@
 # frozen_string_literal: false
 
-# Ruby 1.9.3 and old RubyGems don't play nice with frozen version strings
-# rubocop:disable MutableConstant
-
 module Bundler
-  VERSION = "2.1.0.pre.1"
+  VERSION = "2.1.0.pre.1".freeze
 
   def self.bundler_major_version
     @bundler_major_version ||= VERSION.split(".").first.to_i
